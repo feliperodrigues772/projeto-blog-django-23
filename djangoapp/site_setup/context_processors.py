0f0695b1493eecd1ffdda8c8d583eb1d78accadf
Felipe Rodrigues <felipecb500@gmail.com>
2023-11-02 12:05:23 -0300

    At 20231102

---
+++ djangoapp/site_setup/context_processors.py
@@ -0,0 +1,17 @@
+from site_setup.models import SiteSetup
+
+
+def context_processor_example(request):
+    return {
+        'example': 'Veio  do context'
+    }
+
+def site_setup(request):
+    setup = SiteSetup.objects.order_by('-id').first()
+    return {
+        'site_setup': setup
+    }
+    
+    # return {
+    #     'site_setup': 'Site_setup'
+    # }
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,5 +1,7 @@
 # types: ignore 
 from django.db import models
+from utils.images import resize_image
+from utils.model_validators import validate_png
 
 
 class MenuLink(models.Model):
@@ -21,7 +23,7 @@ class MenuLink(models.Model):
 class SiteSetup(models.Model):
     class Meta:
         verbose_name = 'Setup'
-        verbose_name_plural = 'Setups'
+        verbose_name_plural = 'Setup'
     
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=255)
@@ -31,6 +33,25 @@ class SiteSetup(models.Model):
     show_descrition = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%M',
+        blank=True, default='',
+        validators=[
+            validate_png
+        ],
+    )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
